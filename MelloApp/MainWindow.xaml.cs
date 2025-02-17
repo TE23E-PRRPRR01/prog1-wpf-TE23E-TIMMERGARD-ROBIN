9522837ed725ca8d481df671c4f382a682fd060f
Robin Timmergård. <robin.timmergard@elev.ga.ntig.se>
2025-02-17 13:21:30 +0100

    MELLO

---
+++ MelloApp/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelloApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+        int antalGul = 0;
+        int antalLila = 0;
+        int antalRöd = 0;
+        int antalBlå = 0;
+        int antalGrön = 0;
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void KlickRösta(object sender, RoutedEventArgs e)
+    {
+        // För varje klick +1
+        if (sender == röd)
+        {
+            antalRöd++;
+        }
+        else if (sender == grön)
+        {
+            antalGrön++;
+        }
+        else if (sender == blå)
+        {
+            antalBlå++;
+        }
+        else if (sender == gul)
+        {
+            antalGul++;
+        }
+        else if (sender == lila)
+        {
+            antalLila++;
+        }
+        else if (sender == rensa)
+        {
+            antalGul = 0;
+            antalLila = 0;
+            antalRöd = 0;
+            antalBlå = 0;
+            antalGrön = 0;
+        }
+
+        txbResultat.Text = $"Röd: {antalRöd}, Blå: {antalBlå}, Grön: {antalGrön}, Gul: {antalGul}, Lila: {antalLila}";
+    }
+    
+    
+}
---
+++ SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        // Läsa av maxvärde
+        if (!int.TryParse(tbxMax.Text, out int max))
+        {
+            max = 101;
+        }
+
+        // Slumpa ett talk 1-100
+        int slumptal = Random.Shared.Next(1, max + 1);
+
+        // Skriv ut slumptalet i textboxen
+        txbResultat.Text = $"{slumptal}";
+
+    }
+}
